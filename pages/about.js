9275251c59aa9c0bb34edf9d3c17b874e4675b36
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-18 13:23:42 +0530

    (feat): create menu system 🚚

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 const About = () => (
     <div>
+        <ul>
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+        </ul>
         <h1>About BIZBIZ</h1>
         <p>Application to view Bitcoin Prices.</p>
     </div>
