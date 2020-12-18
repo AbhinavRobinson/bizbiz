6ec41d7c6899555c72df70582667c4bb0fa3c450
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-18 17:18:18 +0530

    (fix): DOM fix 🤔

--- components/Prices.js
+++ components/Prices.js
@@ -10,19 +10,19 @@ class Prices extends React.Component {
 
         if (this.state.currency === 'USD') {
             list = <li className="list-group-item">
-                Bitcoin rate for {this.props.bpi.bpi.USD.description} : <span class="badge badge-primary">{this.props.bpi.bpi.USD.code}</span> <strong>{this.props.bpi.bpi.USD.rate}</strong>
+                Bitcoin rate for {this.props.bpi.bpi.USD.description} : <span className="badge badge-primary">{this.props.bpi.bpi.USD.code}</span> <strong>{this.props.bpi.bpi.USD.rate}</strong>
             </li>
         } else if (this.state.currency === 'GBP') {
             list = <li className="list-group-item">
-                Bitcoin rate for {this.props.bpi.bpi.GBP.description} : <span class="badge badge-primary">{this.props.bpi.bpi.GBP.code}</span> <strong>{this.props.bpi.bpi.GBP.rate}</strong>
+                Bitcoin rate for {this.props.bpi.bpi.GBP.description} : <span className="badge badge-primary">{this.props.bpi.bpi.GBP.code}</span> <strong>{this.props.bpi.bpi.GBP.rate}</strong>
             </li>
         } else if (this.state.currency === 'EUR') {
             list = <li className="list-group-item">
-                Bitcoin rate for {this.props.bpi.bpi.EUR.description} : <span class="badge badge-primary">{this.props.bpi.bpi.EUR.code}</span> <strong>{this.props.bpi.bpi.EUR.rate}</strong>
+                Bitcoin rate for {this.props.bpi.bpi.EUR.description} : <span className="badge badge-primary">{this.props.bpi.bpi.EUR.code}</span> <strong>{this.props.bpi.bpi.EUR.rate}</strong>
             </li>
         } else {
             list = <li className="list-group-item">
-                Bitcoin rate for {this.props.bpi.bpi.USD.description} : <span class="badge badge-primary">{this.props.bpi.bpi.USD.code}</span> <strong>{this.props.bpi.bpi.USD.rate}</strong>
+                Bitcoin rate for {this.props.bpi.bpi.USD.description} : <span className="badge badge-primary">{this.props.bpi.bpi.USD.code}</span> <strong>{this.props.bpi.bpi.USD.rate}</strong>
             </li>
         }
 
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 const About = () => (
     <Layout>
-        <div>
+        <div className="container">
             <h1>About BIZBIZ</h1>
             <p>Application to view Bitcoin Prices.</p>
         </div>
